8817590ba9bb4f19cf364fd4f8253251c7f6d5f0
FetusCoder <127114506+FetusCoder@users.noreply.github.com>
2023-04-24 16:50:20 +0700

    Rewrote javascript because previous logic flawed

--- README.md
+++ README.md
@@ -10,3 +10,21 @@ Added some event listeners to test buttons
 Figuring out how to trigger + operator between two numbers
 Figuring out how to display 2 when 1 + 1 is pressed
 1 + 1 - 1 computes as 1 - 1 instead
+Got stuck, rewriting everything from scratch
+Create calculator object with four properties
+Create display function to display numbers
+Create keys event listener to track clicks on each button
+Use console.log for button clicks for now
+Create inputDigit function to display values of digits and operators
+Update console.log in keys event listener to trigger inputDigit function and updateDisplay
+Create inputDecimal function to display decimal
+update console.log in keys event listener to trigger inputDecimal function and updateDisplay
+Create handleOperator function to enable operators to separate digit clicks and operator clicks
+Fix handleOperator function appending numbers incorrectly to display instead of adding by adding a condition to inputDigit
+Include property of waitingForSecondOperand to destructured calculator to switch it to false from true so the value gets overwritten or appended instead of only appending
+Create operate function to enable the four original functions: add, subtract, multiply and divide
+firstOperand, secondOperand and operator are passed as parameters onto operate function
+If the operator is =, return the secondOperand
+Update handleOperator with an else if condition after isNaN check to check for an existing operator and if so invoke operate, store it in result variable
+Also display result in displayValue for user and update firstOperand to result
+//TODO --> Consecutive operators, clear function, decimal bug, refactors
--- script.js
+++ script.js
@@ -1,24 +1,145 @@
-const addBtn = document.querySelector("#add");
-const subtractBtn = document.querySelector("#subtract");
-const multiplyBtn = document.querySelector("#multiply");
-const divideBtn = document.querySelector("#divide");
-const oneBtn = document.querySelector("#one");
-const twoBtn = document.querySelector("#two");
-const threeBtn = document.querySelector("#three");
-const backSpaceBtn = document.querySelector("#backSpace");
-const fourBtn = document.querySelector("#four");
-const fiveBtn = document.querySelector("#five");
-const sixBtn = document.querySelector("#six");
-const clearBtn = document.querySelector("#clear");
-const sevenBtn = document.querySelector("#seven");
-const eightBtn = document.querySelector("#eight");
-const nineBtn = document.querySelector("#nine");
-const equalBtn = document.querySelector("#equal");
-const zeroBtn = document.querySelector("#zero");
-const displayTotal = document.querySelector("#displayTotal");
-const displaySequence = document.querySelector("displaySequence");
+const calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
-clearBtn.addEventListener("click", () => {
+function updateDisplay() {
+  const displayTotal = document.querySelector("#displayTotal");
+  displayTotal.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector(".grid-keys");
+
+keys.addEventListener("click", (event) => {
+  const { target } = event;
+  const { value } = target;
+
+  if (!target.matches("button")) {
+    return;
+  }
+
+  switch (value) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case "=":
+      handleOperator(value);
+      break;
+    case ".":
+      inputDecimal(value);
+      break;
+    case "clear":
+      clear();
+      break;
+    default:
+      //Check for integer
+      if (Number.isInteger(parseFloat(value))) {
+        inputDigit(value);
+      }
+  }
+
+  updateDisplay();
+
+  /* if (target.classList.contains("operator")) {
+    handleOperator(target.value);
+    updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains("decimal")) {
+    inputDecimal(target.value);
+    updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains("clear")) {
+    clear();
+    updateDisplay();
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay(); */
+});
+
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue =
+      displayValue === "0" ? digit : displayValue + digit;
+  }
+
+  console.log(calculator);
+}
+
+function inputDecimal(dot) {
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = "0.";
+    calculator.waitingForSecondOperand = false;
+    return;
+  }
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+}
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator;
+  const inputValue = parseFloat(displayValue);
+
+  //Checks if operator already exist
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    console.log(calculator);
+    return;
+  }
+
+  //isNaN(value) method checks for number or not
+  if (firstOperand === null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = operate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = `${parseFloat(result.toFixed(5))}`;
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+
+function operate(firstOperand, secondOperand, operator) {
+  if (operator === "+") {
+    return firstOperand + secondOperand;
+  } else if (operator === "-") {
+    return firstOperand - secondOperand;
+  } else if (operator === "*") {
+    return firstOperand * secondOperand;
+  } else if (operator === "/") {
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
+}
+
+function clear() {
+  calculator.displayValue = "0";
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+  console.log(calculator);
+}
+
+/* clearBtn.addEventListener("click", () => {
   total = null;
   displayTotal.textContent = "";
   state = "waiting";
@@ -26,9 +147,11 @@ clearBtn.addEventListener("click", () => {
 
 let state = "waiting";
 let total = null;
+let operator = null;
 
 addBtn.addEventListener("click", () => {
   if (state === "waitingForNextNumber") {
+    operator = "+";
     const currentValue = parseInt(displayTotal.textContent);
     if (total === null) {
       total = currentValue;
@@ -46,9 +169,10 @@ subtractBtn.addEventListener("click", () => {
     if (total === null) {
       total = currentValue;
     } else {
-      total = `${total}` - currentValue;
+      total = total - currentValue;
     }
     displayTotal.textContent = total;
+    operator = "-";
     state = "waiting";
   }
 });
@@ -57,10 +181,11 @@ oneBtn.addEventListener("click", () => {
   if (state === "waiting") {
     displayTotal.textContent = "1";
     state = "waitingForNextNumber";
-  } else if (state === "waitingForNextNumber") {
+  } else {
     displayTotal.textContent += "1";
+    state = "waitingForNextNumber";
   }
-});
+}); */
 
 /* twoBtn.addEventListener("click", () => {
   if (state === "waiting") {
@@ -70,14 +195,6 @@ oneBtn.addEventListener("click", () => {
     displayTotal.textContent += "2";
   }
 }); */
-
-threeBtn.addEventListener("click", () => {
-  displayTotal.textContent += "3";
-});
-
-function operate(operation) {
-  if (operation === "addBtn") return add;
-}
 
 function add(a, b) {
   return a + b;
